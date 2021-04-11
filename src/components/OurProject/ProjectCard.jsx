6cffb195ca9414838e18cc8f8f68711baa52f40b
Dat Pham <duydatyds@gmail.com>
2021-04-12 03:21:32 +1200

    implement project popup images

--- src/components/OurProject/ProjectCard.jsx
+++ src/components/OurProject/ProjectCard.jsx
@@ -21,23 +21,45 @@ import { Card, Icon } from 'semantic-ui-react'
 //   </div>
 // </a>
 // )
+class ProjectCard extends React.Component {
+  componentDidMount() {
+    this.magnificPopup(this.props);
+  }
 
-const ProjectCard = (props) => (
-  <div className="our-products-card">
-  <div className="ui card">
-    <div className="image">
-      <img src={props.image} style={{ height:'300px' }} />
-    </div>
-    <div className="content">
-      <div className="header">{props.name}</div>
-      <div className="meta"><span className="date">{props.address}</span></div>
-      {/* <div className="description">{props.address}</div> */}
-    </div>
-    <div className="extra content">
-      <a><i aria-hidden="true" className="image icon"></i>{props.images.length} Photos</a>
-    </div>
-  </div>
-  </div>
-)
+  magnificPopup(project) {
+    window.$(`#${project.id}`).magnificPopup({
+      type: 'image',
+      items: project.images.map(img => ({
+        src: img,
+      })),
+      gallery: {
+        enabled: true,
+      },
+    })
+  }
+
+  render() {
+    const props = this.props;
+
+    return (<div className="our-products-card">
+      <div className="ui card">
+        <div className="image">
+          <img src={props.image} style={{ height:'300px' }} />
+        </div>
+        <div className="content">
+          <div className="header">{props.name}</div>
+          <div className="meta"><span className="date">{props.address}</span></div>
+          {/* <div className="description">{props.address}</div> */}
+        </div>
+        <div className="extra">
+          <a id={props.id}
+            onClick={() => this.magnificPopup(props)}>
+            <i aria-hidden="true" className="image icon"></i>{props.images.length} Photos
+          </a>
+        </div>
+      </div>
+    </div>)
+  }
+}
 
 export default ProjectCard
